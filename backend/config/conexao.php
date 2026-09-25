f3864c7521ead7ddd092982783be771e78599dc9
Davi Antonio Santos <davi.a.santos25@aluno.senai.br>
2026-09-25 15:43:40 -0300

    ALTERAÇÃO NA CONEXAO COM O BANCO

--- backend/config/conexao.php
+++ backend/config/conexao.php
@@ -1,19 +1,16 @@
 <?php
 
-$host = "10.140.169.23";
+$host = "10.140.169.34";
 $usuario = "root";
 $senha = "123456";
 $banco = "marketplace_rural";
 
-$conexao = new mysqli(
-    $host,
-    $usuario,
-    $senha,
-    $banco
-);
+// --- LINHA QUE ESTAVA FALTANDO ---
+$conexao = new mysqli($host, $usuario, $senha, $banco);
 
+// Verifica se ocorreu algum erro ao tentar conectar
 if ($conexao->connect_error) {
-    throw new RuntimeException("Erro na conexão com o banco.");
+    throw new RuntimeException("Erro na conexão com o banco: " . $conexao->connect_error);
 }
 
 $conexao->set_charset("utf8mb4");
